7a6212a66646cc33386b378cbd9438b803aec867
Aneesh s <aneeshsolomon4@gmail.com>
2026-04-27 11:17:25 +0530

    refactor: remove IRefreshTokenService interface as part of code cleanup

--- UserManagementService.Application/Services/IRefreshTokenService.cs.cs
+++
@@ -1,8 +0,0 @@
-﻿using UserManagementService.Application.DTOs.Auth;
-
-namespace UserManagementService.Application.Services;
-
-public interface IRefreshTokenService
-{
-    Task<LoginResponse> RefreshTokenAsync(string refreshToken);
-}
